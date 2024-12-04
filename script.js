f4346b3b21c6087b3af3352b29d80a2518ff4439
sumit bharti <sumitbharti230@gmail.com>
2024-12-04 15:28:01 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,123 @@
+// for(let i = 0;i<20;i++){
+//     console.log("my name is sumit bharti")
+// }
+
+// let sum = 0;
+//  for (let i = 0; i<=20;i++){
+//     sum = sum + i;
+//     console.log(sum)
+//  }
+
+//  it is for counting even numbers
+// let counteven = 0;
+// for(let i = 1;i <=20;i++){
+//     if (i % 2 == 0) {
+//         counteven ++;
+//     }
+//     console.log(counteven)
+// }
+
+// sum of the even numbers
+// let sumeven = 0;
+// for(let i = 1;i <=20;i++){
+//     if (i % 2 == 0) {
+//         sumeven = sumeven + i
+//     }
+// }
+// console.log("the sum of even number is " + sumeven)
+
+// sum of the odd numbers
+// let sumodd = 0;
+// for(let i = 1;i <=100; i++){
+//     if (i % 2 == 1) {
+//         sumodd = sumodd + i
+//     }
+// }
+// console.log("the sum of odd number is " + sumodd)
+
+
+// table of five
+// let r = 1
+// for (let i = 1; i <= 10; i ++){
+//     r = i*5
+//     console.log(5 + "*"+ i+ "=" + r ) this is my own way 
+
+//     console.log(`19 * ${i} = ${i*19}`) this is sir's way
+
+// }
+
+
+// // 6. Count Down from 20 to 1
+// for (let i = 20; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// 7. Print Multiples of 3
+// for (let i = 1; i <= 10; i++) {
+//     console.log(3 * i);
+// }
+
+
+// 8. Print Squares of Numbers
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i ** 2);
+// }
+
+
+// // 9. Sum of First 100 Numbers
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+// console.log(sum)
+
+
+// 10. Print Cube of Numbers
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i ** 3);
+// }
+
+// 11. Print Numbers in Reverse Order
+// for (let i = 50; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+// 12. Count Down Odd Numbers
+// for (let i = 19; i >= 1; i--) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// // 13. Sum of First 5 Odd Numbers
+// let sum = 0;
+// let count = 0;
+
+// for (let i = 1; count < 5; i++) {
+//     if (i % 2 !== 0) {
+//         sum += i;
+//         count++;
+//     } else {
+//         // Even numbers are ignored
+//         continue;
+//     }
+// }
+
+// console.log(sum);
+
+
+// 14. Print the First 10 Positive Integers
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// 15. Print Powers of 2
+// for (let i = 1; i <= 10; i++) 
+//     {
+//     console.log(2 ** i);
+
+
+
+
